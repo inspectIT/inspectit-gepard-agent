87d0386a7e2ef58261ab121f16c25e4f6c50f62f
EddeCCC <eduard.schander@gmx.de>
2024-07-12 12:34:05 +0200

    remove tests

--- src/test/java/rocks/inspectit/gepard/agent/config/ConfigurationResolverTest.java
+++ src/test/java/rocks/inspectit/gepard/agent/config/ConfigurationResolverTest.java
@@ -1,8 +1,6 @@
 package rocks.inspectit.gepard.agent.config;
 
 import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.SetEnvironmentVariable;
-import org.junitpioneer.jupiter.SetSystemProperty;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,34 +8,14 @@ class ConfigurationResolverTest {
 
     private static final String SERVER_URL_SYSTEM_PROPERTY = "inspectit.config.http.url";
 
-    private static final String SERVER_URL_ENV_PROPERTY = "INSPECTIT_CONFIG_HTTP_URL";
-
-    private static final String TEST_URL_1 = "https://inspectit.rocks/";
-
-    private static final String TEST_URL_2 = "https://inspectit.github.io/inspectit-ocelot";
+    private static final String TEST_URL = "https://inspectit.rocks/";
 
     @Test
-    @SetSystemProperty(key = SERVER_URL_SYSTEM_PROPERTY, value= TEST_URL_1)
     void resolverReturnsUrlIfSystemPropertyExists() {
+        System.setProperty(SERVER_URL_SYSTEM_PROPERTY, TEST_URL);
+
         String url = ConfigurationResolver.getServerUrl();
 
-        assertEquals(TEST_URL_1, url);
-    }
-
-    @Test
-    @SetEnvironmentVariable(key = SERVER_URL_ENV_PROPERTY, value = TEST_URL_1)
-    void resolverReturnsUrlIfEnvPropertyExists() {
-        String url = ConfigurationResolver.getServerUrl();
-
-        assertEquals(TEST_URL_1, url);
-    }
-
-    @Test
-    @SetSystemProperty(key = SERVER_URL_SYSTEM_PROPERTY, value= TEST_URL_1)
-    @SetEnvironmentVariable(key = SERVER_URL_ENV_PROPERTY, value = TEST_URL_2)
-    void resolverReturnsSystemPropertyIfSystemAndEnvPropertyExist() {
-        String url = ConfigurationResolver.getServerUrl();
-
-        assertEquals(TEST_URL_1, url);
+        assertEquals(TEST_URL, url);
     }
 }
