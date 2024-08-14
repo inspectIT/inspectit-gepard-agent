02c660652f5df59e3d2c6952380dcf77a59a2e4b
EddeCCC <eduard.schander@gmx.de>
2024-08-14 09:35:05 +0200

    add docker docs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,5 @@
-rm -rf /agent/*
+rm -rf agent/*
 # Copy agent jar into shared volume
-cp /opentelemetry-javaagent.jar /agent/opentelemetry-javaagent.jar
+cp opentelemetry-javaagent.jar agent/opentelemetry-javaagent.jar
 # Keep the container running
 while true; do sleep 2; done;
