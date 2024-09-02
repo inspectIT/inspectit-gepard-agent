82a3607c2641d859a546093c554e005656674ba7
Levin Kerschberger <Levin.Kerschberger@novatec-gmbh.de>
2024-09-02 16:29:59 +0200

    fix: create docker-img on main-push

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 rm -rf agent/*
 # Copy agent jar into shared volume
+mkdir agent
 cp inspectit-gepard-agent.jar agent/inspectit-gepard-agent.jar
 # Keep the container running
 while true; do sleep 2; done;
