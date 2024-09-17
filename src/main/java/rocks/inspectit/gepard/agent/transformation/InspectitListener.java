3939d35ecbb01d1ee0ec6aed82bc215bb14a5583
EddeCCC <eduard.schander@gmx.de>
2024-09-17 15:26:33 +0200

    add listener

---
+++ src/main/java/rocks/inspectit/gepard/agent/transformation/InspectitListener.java
@@ -0,0 +1,49 @@
+package rocks.inspectit.gepard.agent.transformation;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.utility.JavaModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Callbacks, which are executed before or after {@link DynamicTransformer#transform}. Currently,
+ * just used to log errors after transformation.
+ */
+public class InspectitListener implements AgentBuilder.Listener {
+  private static final Logger log = LoggerFactory.getLogger(InspectitListener.class);
+
+  @Override
+  public void onError(
+      String typeName,
+      ClassLoader classLoader,
+      JavaModule module,
+      boolean loaded,
+      Throwable throwable) {
+    log.error("Dynamic transformation failed for type {} : {}", typeName, throwable.getMessage());
+  }
+
+  @Override
+  public void onDiscovery(
+      String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {}
+
+  @Override
+  public void onTransformation(
+      TypeDescription typeDescription,
+      ClassLoader classLoader,
+      JavaModule module,
+      boolean loaded,
+      DynamicType dynamicType) {}
+
+  @Override
+  public void onIgnored(
+      TypeDescription typeDescription,
+      ClassLoader classLoader,
+      JavaModule module,
+      boolean loaded) {}
+
+  @Override
+  public void onComplete(
+      String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {}
+}
