57af9fd67ddf90c1246f2ab079f12a67b4b6c8b7
EddeCCC <eduard.schander@gmx.de>
2024-11-19 16:43:39 +0100

    refactor MetricFactoryTest

--- inspectit-gepard-agent/src/test/java/rocks/inspectit/gepard/agent/internal/metrics/MetricFactoryTest.java
+++ inspectit-gepard-agent/src/test/java/rocks/inspectit/gepard/agent/internal/metrics/MetricFactoryTest.java
@@ -1,7 +1,7 @@
 /* (C) 2024 */
 package rocks.inspectit.gepard.agent.internal.metrics;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.metrics.ObservableDoubleGauge;
@@ -11,13 +11,18 @@ import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import rocks.inspectit.gepard.agent.internal.otel.OpenTelemetryAccessor;
 
+@ExtendWith(MockitoExtension.class)
 class MetricFactoryTest {
+
+  @Mock private Consumer<ObservableDoubleMeasurement> mockCallback;
 
   // Interval to read and export metrics
   private static final Duration INTERVAL = Duration.ofMillis(500);
@@ -39,23 +44,18 @@ class MetricFactoryTest {
   }
 
   @Test
-  void shouldRecordValueWithCallbackWhenCreatingGauge() throws InterruptedException {
-    AtomicBoolean valueRecorded = new AtomicBoolean(false);
-
-    Consumer<ObservableDoubleMeasurement> callback1 = (measurement) -> valueRecorded.set(true);
-    Consumer<ObservableDoubleMeasurement> callback2 =
+  void shouldRecordValueWithCallbackWhenCreatingGauge() {
+    Consumer<ObservableDoubleMeasurement> errorCallback =
         (measurement) -> {
           throw new RuntimeException("Test exception");
         };
 
     ObservableDoubleGauge gauge1 =
-        MetricFactory.createObservableDoubleGauge("test-gauge", callback1);
+        MetricFactory.createObservableDoubleGauge("test-gauge", mockCallback);
     ObservableDoubleGauge gauge2 =
-        MetricFactory.createObservableDoubleGauge("fail-gauge", callback2);
-    // Wait for MetricReader
-    Thread.sleep(INTERVAL.toMillis() + 100);
+        MetricFactory.createObservableDoubleGauge("fail-gauge", errorCallback);
 
-    assertTrue(valueRecorded.get());
+    verify(mockCallback, timeout(INTERVAL.toMillis() + 100)).accept(any());
 
     gauge1.close();
     gauge2.close();
